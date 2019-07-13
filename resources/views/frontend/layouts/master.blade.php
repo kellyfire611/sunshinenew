7379f9d5ad735acca915fab9d3a888dd8f52a8f5
kellyfire611 <kellyfire611@gmail.com>
2019-07-14 00:02:44 +0700

    Tích hợp AngularJS vào Laravel

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="sunshineApp">
 
 <head>
     <title>@yield('title')</title>
@@ -291,6 +291,22 @@
         });
     </script>
     <script src="{{ asset('themes/cozastore/js/main.js') }}"></script>
+
+    <!-- Include AngularJS -->
+    <script src="{{ asset('vendor/angularjs/angular.min.js') }}"></script>
+    <!-- Include thư viện quản lý Cart - AngularJS -->
+    <script src="{{ asset('vendor/ngCart/dist/ngCart.js') }}"></script>
+
+    <script>
+        // Khởi tạo ứng dụng AngularJS, sử dụng plugin ngCart
+        // Do Laravel và AngularJS đều sử dụng dấu `Double bracket` để render dữ liệu
+        // => để tránh bị xung đột cú pháp, ta sẽ đổi cú pháp render dữ liệu của AngularJS thành <% %>
+        var app = angular.module('sunshineApp', ['ngCart'],
+            function($interpolateProvider) {
+                $interpolateProvider.startSymbol('<%');
+                $interpolateProvider.endSymbol('%>');
+            });
+    </script>
 
     <!-- Các custom script dành riêng cho từng view -->
     @yield('custom-scripts')
