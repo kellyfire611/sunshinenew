8e97b680cf779b6ea0794f313c3c084e74a680b7
kellyfire611 <kellyfire611@gmail.com>
2019-07-21 02:45:17 +0700

    Bổ sung DateRangePicker

--- resources/views/backend/reports/donhang.blade.php
+++ resources/views/backend/reports/donhang.blade.php
@@ -12,16 +12,33 @@ Báo cáo Đơn hàng
 Báo cáo Đơn hàng
 @endsection
 
+@section('custom-css')
+<!-- Các style dành cho thư viện Daterangepicker -->
+<link href="{{ asset('vendor/daterangepicker/daterangepicker.css') }}" type="text/css" rel="stylesheet" />
+
+<style>
+    .notice {
+        font-style: italic;
+        font-size: 0.8em;
+    }
+</style>
+@endsection
+
 @section('content')
 <div class="row">
     <div class="col-md-12">
         <form method="get" action="#" enctype="multipart/form-data">
             {{ csrf_field() }}
             <div class="form-group">
+                <label for="thoigianLapBaoCao">Thời gian lập báo cáo</label>
+                <input type="text" class="form-control" id="thoigianLapBaoCao">
+                <span id="thoigianLapBaoCaoText" class="notice"></span>
+            </div>
+            <div class="form-group" style="display: none;">
                 <label for="tuNgay">Từ ngày</label>
                 <input type="text" class="form-control" id="tuNgay" name="tuNgay">
             </div>
-            <div class="form-group">
+            <div class="form-group" style="display: none;">
                 <label for="denNgay">Đến ngày</label>
                 <input type="text" class="form-control" id="denNgay" name="denNgay">
             </div>
@@ -35,6 +52,71 @@ Báo cáo Đơn hàng
 @endsection
 
 @section('custom-scripts')
+<!-- Các script dành cho thư viện Daterangepicker -->
+<script type="text/javascript" src="{{ asset('vendor/momentjs/moment.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vendor/daterangepicker/daterangepicker.min.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        $('#thoigianLapBaoCao').daterangepicker({
+            "showWeekNumbers": true,
+            "showISOWeekNumbers": true,
+            "timePicker": true,
+            "timePicker24Hour": true,
+            "locale": {
+                "format": "DD/MM/YYYY HH:mm:ss",
+                "separator": " - ",
+                "applyLabel": "Đồng ý",
+                "cancelLabel": "Hủy",
+                "fromLabel": "Từ",
+                "toLabel": "Đến",
+                "customRangeLabel": "Tùy chọn",
+                "weekLabel": "T",
+                "daysOfWeek": [
+                    "CN",
+                    "T2",
+                    "T3",
+                    "T4",
+                    "T5",
+                    "T6",
+                    "T7"
+                ],
+                "monthNames": [
+                    "Tháng 1",
+                    "Tháng 2",
+                    "Tháng 3",
+                    "Tháng 4",
+                    "Tháng 5",
+                    "Tháng 6",
+                    "Tháng 7",
+                    "Tháng 8",
+                    "Tháng 9",
+                    "Tháng 10",
+                    "Tháng 11",
+                    "Tháng 12",
+                ],
+                "firstDay": 1
+            },
+            "startDate": "15/07/2019",
+            "endDate": "21/07/2019",
+            ranges: {
+                'Hôm nay': [moment(), moment()],
+                'Hôm qua': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                '7 ngày gần nhất': [moment().subtract(6, 'days'), moment()],
+                '30 ngày gần nhất': [moment().subtract(29, 'days'), moment()],
+                'Tháng này': [moment().startOf('month'), moment().endOf('month')],
+                'Tháng rồi': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, function(start, end, label) {
+            // Hiển thị thời gian đã chọn
+            $('#thoigianLapBaoCaoText').html('Dữ liệu sẽ được thống kê từ <span style="font-weight: bold">' + start.format('DD/MM/YYYY, HH:mm') + '</span> đến <span style="font-weight: bold">' + end.format('DD/MM/YYYY, HH:mm') + '</span><br />Thời gian lập báo cáo có thể tốn vài phút, vui lòng bấm nút <span style="font-weight: bold">"Lập báo cáo"</span> và chờ đợi trong giây lát!');
+
+            // Gán giá trị cho Ngày để gởi dữ liệu về Backend
+            $('#tuNgay').val(start.format('YYYY-MM-DD HH:mm:ss'));
+            $('#denNgay').val(end.format('YYYY-MM-DD HH:mm:ss'));
+        });
+    });
+</script>
+
 <!-- Các script dành cho thư viện Numeraljs -->
 <script src="{{ asset('vendor/numeraljs/numeral.min.js') }}"></script>
 <script>
@@ -82,7 +164,7 @@ Báo cáo Đơn hàng
                     var myLabels = [];
                     var myData = [];
                     $(response.data).each(function() {
-                        myLabels.push((this.thoiGian));
+                        myLabels.push(moment(this.thoiGian).format('DD/MM/YYYY'));
                         myData.push(this.tongThanhTien);
                     });
                     myData.push(0); // creates a '0' index on the graph
@@ -115,11 +197,21 @@ Báo cáo Đơn hàng
                                 text: "Báo cáo đơn hàng"
                             },
                             scales: {
+                                xAxes: [{
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: 'Ngày nhận đơn hàng'
+                                    }
+                                }],
                                 yAxes: [{
                                     ticks: {
                                         callback: function(value) {
                                             return numeral(value).format('0,0 $')
                                         }
+                                    },
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: 'Tổng thành tiền'
                                     }
                                 }]
                             },
